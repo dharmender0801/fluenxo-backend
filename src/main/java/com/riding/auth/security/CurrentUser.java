1a59e64c3cfc653f5f7d3c19d665663557ddfb1c
dharmender-als <dharmender.kumar@apollosupplychain.com>
2025-06-04 23:54:04 +0530

    changes added

---
+++ src/main/java/com/riding/auth/security/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.riding.auth.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
+
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
