e43999b69c125addb1da0c1c6b273d13a007bfcb
dharmender-als <dharmender.kumar@apollosupplychain.com>
2025-09-08 14:44:21 +0530

    added image url detail

--- src/main/java/com/asc/auth/service/CustomOidcUserService.java
+++ src/main/java/com/asc/auth/service/CustomOidcUserService.java
@@ -48,7 +48,7 @@ public class CustomOidcUserService extends OidcUserService {
 	private OidcUser processOidcUser(OidcUserRequest userRequest, OidcUser oidcUser) {
 		OAuth2UserInfo userInfo = OAuth2UserInfoFactory
 				.getOAuth2UserInfo(userRequest.getClientRegistration().getRegistrationId(), oidcUser.getAttributes());
-		log.info("User Detail : {} ", userInfo);
+		log.info("Img Url : {} ", userInfo.getImageUrl());
 		if (ObjectUtils.isEmpty(userInfo.getEmail())) {
 			throw new OAuth2AuthenticationProcessingException("Email not found from OIDC provider");
 		}
